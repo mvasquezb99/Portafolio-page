fc3cbdbe8439ad5308366a77c47788324f0d214b
mvasquezb99 <mvasquezb@eafit.edu.co>
2024-06-20 13:20:11 -0500

    styles: spacing changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,21 +2,20 @@ import React from "react";
 
 function Navbar() {
     return (
-        <nav className="w-full h-11 flex flex-row justify-center top-0 backdrop-blur-md bg-white/20 sticky z-50">
+        <nav className="w-full h-16 flex flex-row justify-center top-0 backdrop-blur-md bg-white/5 sticky z-50">
             <div className="flex justify-evenly w-96">
-                <a href="#experience" className="flex flex-row items-center transition border-b border-transparent ease-linear delay-100 hover:border-current">
+                <a href="#experience" className=" text-xl flex flex-row items-center transition border-b-2 border-transparent ease-linear delay-100 hover:border-current">
                     Projection
                 </a>
-                <a href="#home" className="flex flex-row items-center transition border-b border-transparent ease-linear delay-100 hover:border-current ">
+                <a href="#home" className=" text-xl flex flex-row items-center transition border-b-2 border-transparent ease-linear delay-100 hover:border-current ">
                     Home
                 </a>
-                <a href="#aboutMe" className="flex flex-row items-center transition border-b border-transparent ease-linear delay-100 hover:border-current ">
+                <a href="#aboutMe" className=" text-xl flex flex-row items-center transition border-b-2 border-transparent ease-linear delay-100 hover:border-current ">
                     About me
                 </a>
             </div>
         </nav>
     )
-}
+} 
 
-
-export default Navbar;
+export default Navbar; 
