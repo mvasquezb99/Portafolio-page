eab2bbbbcc30c342294087f71e12b3d72ac18009
mvasquezb99 <mvasquezb@eafit.edu.co>
2024-06-20 14:07:12 -0500

    refactor: Changed the way svgs where used

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,20 @@ import RoadMap from './components/RoadMap'
 import Projects from './components/Projects'
 import AboutMe from './components/AboutMe'
 import Footer from './components/Footer'
+import './styles/App.css'
 function App() {
   return (
     <>
       <Navbar />
       <main className="flex h-max w-full flex-col items-center">
-        <section id="home" className='w-full h-screen flex flex-col justify-start items-center bg-blueWaves bg-cover aspect-square bg-no-repeat bg-center'>
+        <section id="home" className='w-full h-screen flex flex-col justify-start items-center presContainer'>
           <PresCard />
         </section>
-        <section id="experience" className='w-full h-[1000px] flex flex-col justify-start bg-blueWaves-up bg-cover aspect-square bg-no-repeat bg-center'>
+        <section id="experience" className='w-full h-[1000px] flex flex-col justify-start roadMapContainer'>
           <RoadMap />
           <Projects />
         </section>
-        <section id="aboutMe" className='w-full h-screen flex flex-col justify-start bg-blob-center bg-cover aspect-square bg-no-repeat bg-center'>
+        <section id="aboutMe" className='w-full h-screen flex flex-col justify-start aboutMeContainer'>
           <AboutMe />
         </section>
         <section className=' w-3/5 h-64'>
